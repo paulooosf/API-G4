12fc17e455897410216477f7fb149124b0e0ff62
paulooosf <phpessoal2021@gmail.com>
2024-05-21 17:43:22 -0300

    feat: estruturação inicial das classes

---
+++ src/main/java/br/org/serratec/apig4/ApiG4Application.java
@@ -0,0 +1,13 @@
+package br.org.serratec.apig4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/serratec/apig4/ApiG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.apig4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
