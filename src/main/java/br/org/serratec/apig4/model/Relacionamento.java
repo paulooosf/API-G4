75f2922d9dd7e5d998faeee15735569bbc398e97
paulooosf <phpessoal2021@gmail.com>
2024-05-22 18:58:41 -0300

    refactor: refazendo relacionamento

--- src/main/java/br/org/serratec/apig4/model/Relacionamento.java
+++ src/main/java/br/org/serratec/apig4/model/Relacionamento.java
@@ -1,67 +1,45 @@
 package br.org.serratec.apig4.model;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "relacionamento")
 public class Relacionamento {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_relacionamento")
-	private Long id;
+	@EmbeddedId
+	private RelacionamentoPK id = new RelacionamentoPK();
 	
-	@Column(name = "data_inicio")
-	private LocalDate dataInicioSeguimento;
+	@Column(name = "data_criacao")
+	private LocalDate dataCriacao;
 
-	public Long getId() {
+	public Relacionamento() {
+		
+	}
+	
+	public Relacionamento(RelacionamentoPK id, LocalDate dataCriacao) {
+		super();
+		this.id = id;
+		this.dataCriacao = dataCriacao;
+	}
+
+	public RelacionamentoPK getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(RelacionamentoPK id) {
 		this.id = id;
 	}
 
-	public LocalDate getDataInicioSeguimento() {
-		return dataInicioSeguimento;
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
 	}
 
-	public void setDataInicioSeguimento(LocalDate dataInicioSeguimento) {
-		this.dataInicioSeguimento = dataInicioSeguimento;
-	}
-
-	public Relacionamento() {
-
-	}
-
-	public Relacionamento(Long id, LocalDate dataInicioSeguimento) {
-		super();
-		this.id = id;
-		this.dataInicioSeguimento = dataInicioSeguimento;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Relacionamento other = (Relacionamento) obj;
-		return Objects.equals(id, other.id);
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
 	}
 }
