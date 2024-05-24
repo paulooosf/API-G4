0e14c98d2b6980e02eb4417c95553ff4bc57a1fa
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-24 18:18:53 -0300

    feat: criação da controller e service de Relacionamento

--- src/main/java/br/org/serratec/apig4/controller/RelacionamentoController.java
+++ src/main/java/br/org/serratec/apig4/controller/RelacionamentoController.java
@@ -3,10 +3,17 @@ package br.org.serratec.apig4.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.org.serratec.apig4.dto.RelacionamentoDTO;
+import br.org.serratec.apig4.dto.RelacionamentoInserirDTO;
 import br.org.serratec.apig4.model.Relacionamento;
 import br.org.serratec.apig4.service.RelacionamentoService;
 
@@ -18,8 +25,18 @@ public class RelacionamentoController {
 	private RelacionamentoService relacionamentoService;
 
 	@GetMapping
-	public List<Relacionamento> findAll() {
-		return relacionamentoService.findAll();
+	public ResponseEntity<List<RelacionamentoDTO>> findAll() {
+		return ResponseEntity.ok(relacionamentoService.findAll());
 	}
-
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<List<Relacionamento>> buscarSeguidoresByUsuarioId(@PathVariable Long id){
+		return ResponseEntity.ok(relacionamentoService.buscarSeguidoresByUsuarioId(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<RelacionamentoDTO> inserir(@RequestBody RelacionamentoInserirDTO relacionamentoInserirDTO){
+		RelacionamentoDTO relacionamentoDTO = relacionamentoService.inserir(relacionamentoInserirDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(relacionamentoDTO);
+	}
 }
--- src/main/java/br/org/serratec/apig4/service/RelacionamentoService.java
+++ src/main/java/br/org/serratec/apig4/service/RelacionamentoService.java
@@ -1,12 +1,20 @@
 package br.org.serratec.apig4.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.org.serratec.apig4.dto.RelacionamentoDTO;
+import br.org.serratec.apig4.dto.RelacionamentoInserirDTO;
+import br.org.serratec.apig4.exception.NotFoundException;
 import br.org.serratec.apig4.model.Relacionamento;
+import br.org.serratec.apig4.model.Usuario;
 import br.org.serratec.apig4.repository.RelacionamentoRepository;
+import br.org.serratec.apig4.repository.UsuarioRepository;
 
 @Service
 public class RelacionamentoService {
@@ -14,8 +22,47 @@ public class RelacionamentoService {
 	@Autowired
 	private RelacionamentoRepository relacionamentoRepository;
 
-	public List<Relacionamento> findAll() {
-		return relacionamentoRepository.findAll();
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public List<RelacionamentoDTO> findAll() {
+		List<Relacionamento> relacionamentos = relacionamentoRepository.findAll();
+
+		List<RelacionamentoDTO> relacionamentoDTO = new ArrayList<>();
+
+		for (Relacionamento relacionamento : relacionamentos) {
+			RelacionamentoDTO relacDTO = new RelacionamentoDTO(relacionamento);
+			relacionamentoDTO.add(relacDTO);
+		}
+		return relacionamentoDTO;
 	}
 
+	public List<Relacionamento> buscarSeguidoresByUsuarioId(Long id) {
+		List<Relacionamento> relacionamentos = relacionamentoRepository.buscarSeguidoresByUsuarioId(id);
+		if (relacionamentos.isEmpty()) {
+			throw new NotFoundException();
+		}
+
+		return relacionamentos;
+	}
+
+	public RelacionamentoDTO inserir(RelacionamentoInserirDTO relacionamentoInserirDTO) {
+		// Armazena o iD
+		Optional<Usuario> seguidorOpt = usuarioRepository.findById(relacionamentoInserirDTO.getSeguidorId());
+		Optional<Usuario> seguidoOpt = usuarioRepository.findById(relacionamentoInserirDTO.getSeguidoId());
+
+		// Cria uma instância de Relacionamento
+		Relacionamento relacionamento = new Relacionamento();
+
+		// Chama essa instância e insere os dados armazenados em seguidorOpt e
+		// seguidoOpt
+		relacionamento.getId().setUsuarioSeguidor(seguidorOpt.get());
+		relacionamento.getId().setUsuarioSeguido(seguidoOpt.get());
+		relacionamento.setDataInicio(LocalDate.now());
+
+		relacionamento = relacionamentoRepository.save(relacionamento);
+
+		RelacionamentoDTO relacionamentoDTO = new RelacionamentoDTO(relacionamento);
+		return relacionamentoDTO;
+	}
 }
