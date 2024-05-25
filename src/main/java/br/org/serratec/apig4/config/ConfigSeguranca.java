0c730d4f61e7130f7ab14c498af0efcdea42c097
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-25 17:03:47 -0300

    feat: configurações iniciais de autenticação

---
+++ src/main/java/br/org/serratec/apig4/config/ConfigSeguranca.java
@@ -0,0 +1,58 @@
+package br.org.serratec.apig4.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSeguranca {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+				.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(requests -> {
+					requests.requestMatchers(HttpMethod.GET, "/usuario").permitAll();
+					requests.requestMatchers(HttpMethod.GET, "/relacionamentos").permitAll();
+					requests.requestMatchers(HttpMethod.GET, "/postagem").permitAll();
+					requests.requestMatchers(HttpMethod.GET, "/comentarios").permitAll().anyRequest().authenticated();
+				}).sessionManagement(session -> {
+					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				});
+
+		return http.build();
+	}
+
+	@Bean
+	InMemoryUserDetailsManager userDetailService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("teste").password("123456").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8080/"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration.applyPermitDefaultValues());
+
+		return source;
+	}
+
+}
