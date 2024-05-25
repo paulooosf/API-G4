a0bea43619e07d684da615538458e5b6aed2157a
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-25 18:04:28 -0300

    fix: alteração na classe de configuração e application.properties

--- src/main/java/br/org/serratec/apig4/config/ConfigSeguranca.java
+++ src/main/java/br/org/serratec/apig4/config/ConfigSeguranca.java
@@ -2,6 +2,7 @@ package br.org.serratec.apig4.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,16 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import br.org.serratec.apig4.security.JwtUtil;
+
 @Configuration
 @EnableWebSecurity
 public class ConfigSeguranca {
+
+	@Autowired
+	JwtUtil jwtUtil;
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
