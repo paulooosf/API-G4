b08fbb6a3a8471df9541b6a143c5aee49b463767
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-26 18:47:49 -0300

    feat: mensagem swagger Relacionamento Controller (commit Thiago)

--- src/main/java/br/org/serratec/apig4/controller/RelacionamentoController.java
+++ src/main/java/br/org/serratec/apig4/controller/RelacionamentoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +16,11 @@ import br.org.serratec.apig4.dto.RelacionamentoDTO;
 import br.org.serratec.apig4.dto.RelacionamentoInserirDTO;
 import br.org.serratec.apig4.model.Relacionamento;
 import br.org.serratec.apig4.service.RelacionamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/relacionamentos")
@@ -26,24 +30,29 @@ public class RelacionamentoController {
 	private RelacionamentoService relacionamentoService;
 
 	@GetMapping
+	@Operation(summary = "Lista todas as postagens", description = "A resposta da requisição irá listar todas as postagens")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	public ResponseEntity<List<RelacionamentoDTO>> findAll() {
 		return ResponseEntity.ok(relacionamentoService.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<List<Relacionamento>> buscarSeguidoresByUsuarioId(@PathVariable Long id){
+	public ResponseEntity<List<Relacionamento>> buscarSeguidoresByUsuarioId(@PathVariable Long id) {
 		return ResponseEntity.ok(relacionamentoService.buscarSeguidoresByUsuarioId(id));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<RelacionamentoDTO> inserir(@RequestBody RelacionamentoInserirDTO relacionamentoInserirDTO){
+	@Operation(summary = "Lista todas as postagens", description = "A resposta da requisição irá listar todas as postagens")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
+	public ResponseEntity<RelacionamentoDTO> inserir(@RequestBody RelacionamentoInserirDTO relacionamentoInserirDTO) {
 		RelacionamentoDTO relacionamentoDTO = relacionamentoService.inserir(relacionamentoInserirDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(relacionamentoDTO);
-	}
-	
-	@DeleteMapping("/{idUsuarioSeguidor}/{idUsuarioSeguido}")
-	public ResponseEntity<Void> deletar(@PathVariable Long idUsuarioSeguidor, @PathVariable Long idUsuarioSeguido){
-		relacionamentoService.deletar(idUsuarioSeguidor, idUsuarioSeguido);
-		return ResponseEntity.noContent().build();
+
 	}
 }
