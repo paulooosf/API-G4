d6748499052de90a6b1fe06c91a06622a189ba37
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-27 18:54:45 -0300

    fix: correção de classe antiga

--- src/main/java/br/org/serratec/apig4/controller/ComentarioController.java
+++ src/main/java/br/org/serratec/apig4/controller/ComentarioController.java
@@ -22,42 +22,63 @@ import br.org.serratec.apig4.dto.ComentarioInserirDTO;
 import br.org.serratec.apig4.dto.ComentarioResponseDTO;
 import br.org.serratec.apig4.exception.NotFoundException;
 import br.org.serratec.apig4.model.Comentario;
+import br.org.serratec.apig4.model.Relacionamento;
 import br.org.serratec.apig4.service.ComentarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagem/{postagemId}/comentarios")
 public class ComentarioController {
 
+
 	@Autowired
 	private ComentarioService comentarioService;
 
 	@GetMapping
+	@Operation(summary = "Lista todos os comentários", description = "A resposta da requisição irá listar todos os comentários")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	public ResponseEntity<Page<ComentarioResponseDTO>> listar(
 			@PageableDefault(sort = "horaCriacao", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable pageable,
 			@PathVariable Long postagemId) {
-
-		Page<Comentario> comentarioPage = comentarioService.listar(pageable, postagemId);
+		
+		Page<Comentario> comentarioPage = comentarioService.listar(pageable, postagemId); 
 		Page<ComentarioResponseDTO> comentariosDTOPage = comentarioPage.map(ComentarioResponseDTO::new);
-
+		
 		return ResponseEntity.ok().body(comentariosDTOPage);
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "Busca um comentário por ID", description = "A resposta da requisição irá exibir o comentário de ID especificado")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	public ResponseEntity<ComentarioResponseDTO> buscarPorId(@PathVariable Long id, @PathVariable Long postagemId) {
 
-		try {
-			ComentarioResponseDTO comentarioResponseDTO = new ComentarioResponseDTO(
-					comentarioService.buscarPorId(id, postagemId));
-			return ResponseEntity.ok().body(comentarioResponseDTO);
-
-		} catch (NotFoundException e) {
-			return ResponseEntity.notFound().build();
-		}
-
+		 try {
+			 ComentarioResponseDTO comentarioResponseDTO = new ComentarioResponseDTO(comentarioService.buscarPorId(id, postagemId)); 
+			 return ResponseEntity.ok().body(comentarioResponseDTO);
+		       
+		    } catch (NotFoundException e) {
+		        return ResponseEntity.notFound().build();
+		    }
+		
 	}
 
 	@PostMapping
+	@Operation(summary = "Insere um novo comentário", description = "A requisição irá postar um novo comentário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<ComentarioResponseDTO> inserir(@Valid @RequestBody ComentarioInserirDTO comentarioInserirDTO,
 			@PathVariable Long postagemId) {
@@ -69,28 +90,38 @@ public class ComentarioController {
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "Edita um comentário", description = "A requisição irá editar um comentário já existente")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	public ResponseEntity<ComentarioResponseDTO> editar(@PathVariable Long id, @RequestBody ComentarioDTO comentarioDTO,
 			@PathVariable Long postagemId) {
 		try {
-			comentarioService.editar(id, comentarioDTO, postagemId);
+		comentarioService.editar(id, comentarioDTO, postagemId);
+		
+		Comentario comentario = comentarioService.editar(id, comentarioDTO, postagemId);
+		ComentarioResponseDTO responseDTO = new ComentarioResponseDTO(comentario);
 
-			Comentario comentario = comentarioService.editar(id, comentarioDTO, postagemId);
-			ComentarioResponseDTO responseDTO = new ComentarioResponseDTO(comentario);
-
-			return ResponseEntity.ok().body(responseDTO);
-
+		return ResponseEntity.ok().body(responseDTO);
+		
 		} catch (NotFoundException e) {
-			return ResponseEntity.notFound().build();
-		}
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Remove um comentário", description = "A requisição irá remover um comentário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Relacionamento.class), mediaType = "apllication/json") }),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso") })
 	public ResponseEntity<Void> deletar(@PathVariable Long id, @PathVariable Long postagemId) {
 		try {
 			comentarioService.deletar(id, postagemId);
-			return ResponseEntity.noContent().build();
-		} catch (NotFoundException e) {
-			return ResponseEntity.notFound().build();
-		}
+	        return ResponseEntity.noContent().build();
+	    } catch (NotFoundException e) {
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 }
